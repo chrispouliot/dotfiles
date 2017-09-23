674e6dc45f696e50a54fd96c9b388add34d8334d
Christophe Pouliot <cristophepoug@gmail.com>
2017-09-23 10:35:08 -0700

    Added diff-so-fancy and zsh_history copy

--- setup.sh
+++ setup.sh
@@ -164,6 +164,28 @@ fi
 
 
 # ------------
+# GIT SO FANCY
+# ------------
+
+brew install diff-so-fancy
+
+git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
+git config --global color.ui true
+
+git config --global color.diff-highlight.oldNormal "red bold"
+git config --global color.diff-highlight.oldHighlight "red bold 52"
+git config --global color.diff-highlight.newNormal "green bold"
+git config --global color.diff-highlight.newHighlight "green bold 22"
+
+git config --global color.diff.meta "227"
+git config --global color.diff.frag "magenta bold"
+git config --global color.diff.commit "227 bold"
+git config --global color.diff.old "red bold"
+git config --global color.diff.new "green bold"
+git config --global color.diff.whitespace "red reverse"
+
+
+# ------------
 # PRIVATE KEYS
 # ------------
 
@@ -175,6 +197,8 @@ ssh-copy-id $localvar
 scp $localvar:~/.ssh/config ~/.ssh/config
 
 scp $localvar:~/.netrc ~/.nerc
+
+scp $localvar:~/.zsh_history ~/.zsh_history
 
 
 # -------------
